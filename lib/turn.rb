affcc0b0167c5bffa056e5b7a7b82a42c5d8b931
scottyd_14 <scottyd_14@hotmail.com>
2018-10-01 17:03:23 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -37,7 +37,11 @@ def turn
   puts "Please enter 1-9:"
   user_input = gets.strip
   input_to_index(user_input)
-  valid_move(board, converted_input)
+  if valid_move(board, converted_input) == FALSE
+    puts "Please pick another position"
+  else 
+    
+  end
   
 end
 
