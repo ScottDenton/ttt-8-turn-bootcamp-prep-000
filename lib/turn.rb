44fcfe855efdc09f71eb3eb1556fa16da3215486
scottyd_14 <scottyd_14@hotmail.com>
2018-10-01 16:45:37 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,7 +16,17 @@ def valid_move? (board, index)
   end
 end
 
-def position_taken?
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil 
+    FALSE
+  else
+    TRUE
+  end
+end
+
+
+def move (board, index, token = "X")
+  
   
   
 end
