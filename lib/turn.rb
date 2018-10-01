4b1d93b1cd2ba3a02f16d69702f14c78362b2023
scottyd_14 <scottyd_14@hotmail.com>
2018-10-01 16:43:16 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,20 @@ def display_board(board)
   puts "-----------"
   puts "   |   |   "
 end
+
+def valid_move? (board, index)
+  if index.to_i >= 0 && index.to_i < 9
+    if !position_taken?
+    TRUE
+  else  
+    FALSE
+    end
+  end
+end
+
+def position_taken?
+  
+  
+end
+
+
